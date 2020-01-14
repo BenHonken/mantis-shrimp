b7dff89c9f06092280f8d02ce1a397de85df55dc
Ben Honken <honkenb@gmail.com>
2020-01-13 18:17:33 -0600

    updated api routes and student

--- public/assets/js/student.js
+++ public/assets/js/student.js
@@ -1,6 +1,11 @@
 $.get("/api/user/").then(function(data) {
     var studentName = $("#studentName");
     studentName.text(data.first_name + " " + data.last_name);
+    if(data.tutor_id){
+        $.get("api/get_user_by_id/", data.tutor_id).then(function(results){
+        $("#tutorName").text(results.first_name + " " + results.last_name)
+        })
+    }
 });
 
 $.get("/api/user_hours").then(function(data) {
