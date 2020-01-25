2a477510e3ee84e21ae809f46a11bec9bf88582d
kbetlach <kbetlach@gmail.com>
2020-01-25 12:06:54 -0600

    new ben pic

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ if (process.env.JAWSDB_URL) {
         host: "localhost",
         port: 3306,
         user: "root",
-        password: null,
+        password: "rootroot",
         database: "tutordb"
     });
 }
